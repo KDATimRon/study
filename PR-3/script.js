2203df2ee558df24808109a1fef89243aa3dfc61
Dmitry <itproweb@yandex.ru>
2021-09-12 14:18:36 +0300

    drag-drop

--- PR-3/script.js
+++ PR-3/script.js
@@ -1,36 +1,3 @@
-let setImages = {
-  tree: 'дерево',
-  iron: 'утюг',
-  dog: 'собака',
-  mittens: 'варежки',
-  cabbage: 'капуста',
-  watermelon: 'арбуз',
-  flower: 'цветок',
-  plate: 'тарелка',
-  umbrella: 'зонд',
-  shorts: 'шорты',
-  car: 'машина',
-  apple: 'яблоко',
-  frog: 'лягушка',
-  watch: 'часы',
-  threads: 'нитки',
-  butterfly: 'бабочка',
-  sock: 'носок',
-  icecream: 'мороженое',
-  raccoon: 'енот',
-  train: 'поезд',
-  fridge: 'холодильник',
-  pear: 'груша',
-  brush: 'щетка',
-  skirt: 'юбка',
-  beatle: 'жук',
-  apron: 'фартук',
-  indian: 'индеец',
-  christmastree: 'ёлка',
-  excavator: 'экскаватор',
-  fish: 'рыба',
-};
-
 let btnRun = $('.big-button');
 let picWrapper = $('.pic-wrapper');
 let abc = $('.abc');
@@ -49,20 +16,7 @@ let getImageRandom = (obj, count) => {
 };
 let showPictures = async () => {
   let wrapper = $('.wrapper');
-  /*let picBlock = $('.pic-block');
-  let dragBlock = $('.drag-block');
-  picBlock.animate({ opacity: 0 });
-  dragBlock.animate({ opacity: 0 });*/
   wrapper.animate({ opacity: 0 });
-
-  /*wrapper.each((i, item) => {
-    setTimeout(() => {
-      $(item).animate({ opacity: 1 }, 2000);
-    }, i * 800);
-  });
-  setTimeout(() => {
-    createAbcBlock();
-  }, 5000);*/
 
   for (let i = 0; i < wrapper.length; i++) {
     await $(wrapper[i]).animate({ opacity: 1 }, 500).promise();
@@ -82,42 +36,7 @@ let createPictures = (Obj, count) => {
   }
 };
 
-let createAbcBlock = () => {
-  let arrAbc = [
-    'а',
-    'б',
-    'в',
-    'г',
-    'д',
-    'е',
-    'ё',
-    'ж',
-    'з',
-    'и',
-    'й',
-    'к',
-    'л',
-    'м',
-    'н',
-    'о',
-    'п',
-    'р',
-    'с',
-    'т',
-    'у',
-    'ф',
-    'х',
-    'ц',
-    'ч',
-    'ш',
-    'щ',
-    'ъ',
-    'ы',
-    'ь',
-    'э',
-    'ю',
-    'я',
-  ];
+let createAbcBlock = async () => {
   let tempAbc = document.querySelector('#temp-abc').content;
   let blockAbc = tempAbc.querySelector('.letter');
   abc.animate({ opacity: 1 }, 1500);
@@ -127,15 +46,101 @@ let createAbcBlock = () => {
     let newImg = newEl.querySelector('img');
     newImg.src = `img/lit-${i + 1}.png`;
     newEl.dataset.litter = arrAbc[i];
+
     abc.append(newEl);
+    await $(newEl).addClass('scale').promise();
   }
+  let letter = $('.letter');
+  setInterval(async () => {
+    let index = Math.floor(Math.random() * letter.length);
+    $(letter[index]).addClass('rotate');
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    $(letter[index]).removeClass('scale rotate');
+  }, 5000);
+  onEventElement();
 };
 
 createPictures(setImages, 6);
 
-btnRun.on('click', (evt) => {
-  $(evt.target).fadeOut(1000);
+btnRun.on('click', (event) => {
+  $(event.target).fadeOut(1000);
   picWrapper.animate({ opacity: 1 }, 1500);
 
   showPictures();
 });
+
+let onEventElement = () => {
+  let arrBlockDrag = document.querySelectorAll('.letter');
+  for (let i = 0; i < arrBlockDrag.length; i++) {
+    getDragDropElement(arrBlockDrag[i]);
+  }
+};
+
+let getDragDropElement = (element) => {
+  let currentDroppable = null;
+
+  element.classList.remove('scale');
+
+  element.onmousedown = (event) => {
+    //let shiftX = event.clientX - element.getBoundingClientRect().left;
+    //let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+    element.style.cssText = 'position: absolute; z-index: 100;';
+    /* document.body.append(element);*/
+
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+      element.style.left = pageX - 370 + 'px';
+      element.style.top = pageY - 55 + 'px';
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+
+      element.hidden = true;
+      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+      element.hidden = false;
+
+      if (!elemBelow) return;
+
+      let droppableBelow = elemBelow.closest('.drag-block');
+      if (currentDroppable != droppableBelow) {
+        if (currentDroppable) {
+          // null если мы были не над droppable до этого события
+          // (например, над пустым пространством)
+          leaveDroppable(currentDroppable);
+        }
+        currentDroppable = droppableBelow;
+        if (currentDroppable) {
+          // null если мы не над droppable сейчас, во время этого события
+          // (например, только что покинули droppable)
+          enterDroppable(currentDroppable, element);
+        }
+      }
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    element.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      element.onmouseup = null;
+    };
+  };
+  function enterDroppable(elem, litter) {
+    console.log(elem.dataset.litter);
+    console.log(litter.dataset.litter);
+    if (elem.dataset.litter === litter.dataset.litter) {
+      elem.style.cssText = 'background: rgb(179 231 100);';
+    } else {
+      elem.style.cssText = 'background: rgb(255 100 114);';
+    }
+  }
+
+  function leaveDroppable(elem) {
+    elem.style.background = '';
+  }
+  element.ondragstart = () => false;
+};
